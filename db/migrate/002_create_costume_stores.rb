a3b8ea17195079338b8d7f184ff83820983233a9
Sushi Chanrai <sushi@pathworks.co>
2018-08-28 14:44:01 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
+      t.string :location
+      t.integer :costume_inventory
       t.float :price
       t.text :size
       t.text :image_url
@@ -10,5 +12,14 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     end
   end
 
-
 end
+
+
+
+name
+location
+number of costumes, or "costume inventory"
+number of employees
+whether or not it's still in business
+opening time
+closing time
