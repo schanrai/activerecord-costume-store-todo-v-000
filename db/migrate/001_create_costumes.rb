55f6998f3660c80765950362c9c7bd98cda3e8ea
Sushi Chanrai <sushi@pathworks.co>
2018-08-28 14:41:21 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,7 +12,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.real :price
+      t.float :price
       t.text :size
       t.text :image_url
       t.timestamps
