e62fc1caad745600f0dfcb4f60eef550775dd6d5
Sushi Chanrai <sushi@pathworks.co>
2018-08-28 14:42:48 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.text :size
+      t.text :image_url
+      t.timestamps
+    end
+  end
+
+
+end
